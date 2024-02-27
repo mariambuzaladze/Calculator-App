01d496b7c75b4b7e9f8d2d302db698e619974ea6
mariambuzaladze <mariambuzaladze10@example.come>
2024-02-28 02:05:11 +0400

    add calculator functionality

--- script.js
+++ script.js
@@ -6,6 +6,13 @@ const output = document.getElementsByClassName("output")[0];
 const deleteButton = document.getElementsByClassName("delete")[0];
 const reset = document.getElementsByClassName("reset")[0];
 const equals = document.getElementsByClassName("equals")[0];
+const numbers = document.getElementsByClassName("number");
+const outputText = document.getElementsByClassName("output-text")[0];
+const plus = document.getElementsByClassName("plus")[0];
+const minus = document.getElementsByClassName("minus")[0];
+const devision = document.getElementsByClassName("devision")[0];
+const mulpitly = document.getElementsByClassName("multiply")[0];
+const period = document.getElementsByClassName("period")[0];
 
 let position = "left";
 
@@ -29,5 +36,66 @@ circle.addEventListener("click", function () {
     body.classList.remove("theme-three");
 
     position = "left";
+  }
+});
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    output.classList.add("active");
+    outputText.textContent += Number(numbers[i].textContent);
+  });
+}
+
+deleteButton.addEventListener("click", function () {
+  outputText.textContent = outputText.textContent.slice(0, -1);
+});
+
+reset.addEventListener("click", function () {
+  outputText.textContent = "";
+});
+
+period.addEventListener("click", function () {
+  if (outputText.textContent !== "") {
+    outputText.textContent += ".";
+  }
+});
+
+plus.addEventListener("click", function () {
+  if (outputText.textContent !== "") {
+    outputText.textContent += "+";
+  }
+});
+
+minus.addEventListener("click", function () {
+  if (outputText.textContent !== "") {
+    outputText.textContent += "-";
+  }
+});
+
+devision.addEventListener("click", function () {
+  if (outputText.textContent !== "") {
+    outputText.textContent += "/";
+  }
+});
+
+mulpitly.addEventListener("click", function () {
+  if (outputText.textContent !== "") {
+    outputText.textContent += "x";
+  }
+});
+
+equals.addEventListener("click", function () {
+  if (outputText.textContent.includes("+")) {
+    let userNumbers = outputText.textContent.split("+");
+    outputText.textContent = Number(userNumbers[0]) + Number(userNumbers[1]);
+  } else if (outputText.textContent.includes("-")) {
+    let userNumbers = outputText.textContent.split("-");
+    outputText.textContent = Number(userNumbers[0]) - Number(userNumbers[1]);
+  } else if (outputText.textContent.includes("/")) {
+    let userNumbers = outputText.textContent.split("/");
+    outputText.textContent = Number(userNumbers[0]) / Number(userNumbers[1]);
+  } else if (outputText.textContent.includes("x")) {
+    let userNumbers = outputText.textContent.split("x");
+    outputText.textContent = Number(userNumbers[0]) * Number(userNumbers[1]);
   }
 });
